f0d6adc6bc042bdf1e3e800fbd90618f5fe8cdb6
biramaster <andre_rubira@hotmail.com>
2015-06-03 15:06:25 +0200

    WindowsGame

---
+++ SideScroller/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SideScroller
+{
+    public partial class Form1 : Form
+    {
+        scene1 sc1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Application.Idle += HandleApplicationIdle;
+            sc1 = new scene1();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int PeekMessage(out NativeMessage message, IntPtr window, uint filterMin, uint filterMax, uint remove);
+
+        private void HandleApplicationIdle(object sender, EventArgs e)
+        {
+            while (IsApplicationIdle())
+            {
+                Update();
+                Render();
+            } 
+        }
+
+        private void Render()
+        {
+            sc1.Draw(this.CreateGraphics());
+        }
+
+        private void Update()
+        {
+          
+        }
+
+        private bool IsApplicationIdle()
+        {
+            NativeMessage result;
+            return PeekMessage(out result, IntPtr.Zero, (uint)0, (uint)0, (uint)0) == 0;
+        }
+    }
+}
---
+++ SideScroller/scene1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideScroller
+{
+    class scene1
+    {
+        public void Draw(Graphics g)
+        {
+            SolidBrush myBrush = new SolidBrush(Color.Red);
+            g.FillRectangle(myBrush, new Rectangle(0, 0, 200, 300));
+        }
+    }
+}
